65479abb225521b395f66cb6b3f8f45f6eff1e4e
superproger3000 <167572559+superproger3000@users.noreply.github.com>
2024-05-11 13:32:56 +0300

    Create NumberProcessor.java

---
+++ NumberProcessor.java
@@ -0,0 +1,13 @@
+public class TestingNumberProcessor {
+    public static void main(String[] args) {
+        int[] numbers = NumberProcessor.readNumbersFromFile("input.txt");
+        int min = NumberProcessor.min(numbers);
+        System.out.println("Минимальное: " + min);
+        int max = NumberProcessor.max(numbers);
+        System.out.println("Максимальное: " + max);
+        int sum = NumberProcessor.sum(numbers);
+        System.out.println("Сумма: " + sum);
+        int mult = NumberProcessor.mult(numbers);
+        System.out.println("Произведение: " + mult);
+    }
+}
